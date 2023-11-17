d44102823be2e7275eb9bdaf21f76d5d9edb329d
Charles <charlesthiagosantos@gmail.com>
2023-11-17 13:58:02 -0300

    feat : Id para ser a chave primaria , para ser incrementado no banco de dados

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -3,12 +3,17 @@ package com.devsuperior.dslist.entities;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name =  "tb_game")
 public class Game {
 	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Integer year;

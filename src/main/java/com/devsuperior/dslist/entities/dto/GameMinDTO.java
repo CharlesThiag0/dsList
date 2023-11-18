153bf9df00b916e50d96a01729c81972829c496c
Charles <charlesthiagosantos@gmail.com>
2023-11-18 14:13:08 -0300

    feat : endPoints custom

--- src/main/java/com/devsuperior/dslist/entities/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/entities/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.entities.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -21,6 +22,14 @@ public class GameMinDTO {
 		shortDescription = entityes.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShotDescription();
+	}
+	
 	public Long getId() {
 		return id;
 	}
